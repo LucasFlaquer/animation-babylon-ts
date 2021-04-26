7fd9deb19d947505d07ee87f0d361a800f68da01
LucasFlaquer <lucas.flaquer@gmail.com>
2021-04-26 20:19:40 -0300

    ground with hightMap

---
+++ src/functions/buildGround.ts
@@ -0,0 +1,27 @@
+import { MeshBuilder, Scene, StandardMaterial, Texture } from "@babylonjs/core";
+const VILLAGE_ASSET = "https://assets.babylonjs.com/environments/valleygrass.png"
+export function buildGround(scene: Scene) {
+
+  const groundMat = new StandardMaterial("groundMat", scene)
+  groundMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/villagegreen.png", scene)
+  groundMat.diffuseTexture.hasAlpha = true
+
+  const ground = MeshBuilder.CreateGround("ground", { width: 24, height: 24 })
+  ground.material = groundMat
+
+  const largeGroundMaterial = new StandardMaterial("largeGroundMat", scene)
+  largeGroundMaterial.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/valleygrass.png", scene)
+
+  const largeGround = MeshBuilder.CreateGroundFromHeightMap(
+    "largeGround",
+    "https://assets.babylonjs.com/environments/villageheightmap.png",
+    { width: 150, height: 150, subdivisions: 20, minHeight: 0, maxHeight: 10 },
+    scene
+  )
+  largeGround.material = largeGroundMaterial
+  largeGround.position.y = -0.01
+
+  return { ground, largeGround }
+
+
+}
